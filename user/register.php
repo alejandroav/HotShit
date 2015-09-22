be6a0ec021eabf090690990dbe9985c30d7eb3f4
alejandroav <alejandroalarcon93@gmail.com>
2015-09-22 20:57:41 +0200

    creacion de registro

---
+++ user/register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<title>User registration</title>
+<body>
+
+<?php 
+	include '../header.php';
+?>
+
+<form>
+	<input type="text" placeholder="Username"></input>
+	<input type="password" placeholder="Password"></input>
+	<input type="submit">Submit</input>
+</form>
+
+</body>
+</html>
