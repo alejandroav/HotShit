2719f76ca9d840f22fee5e12efd058eb02805c63
themarioga <marioga2512@gmail.com>
2015-09-28 17:24:37 +0200

    Update

--- src/index.php
+++ src/index.php
@@ -10,5 +10,30 @@
 	</head>
 	<body>
 		<?php include ("header.php"); ?>
+		<div class="row">
+			<div class="col s4">
+				<h2>Titulo columna</h2>
+				<div class="card green-white lighten-0">
+					<div class="card-content black-text">
+						<span class="card-title black-text">Titulo tarjeta</span>
+						<p>Descripcion</p>
+					</div>
+					<div class="card-action">
+						<a href="#">Favoritos</a>
+						<a href="#">Retweet</a>
+					</div>
+				</div>
+				<div class="card green-white lighten-0">
+					<div class="card-content black-text">
+						<span class="card-title black-text">Titulo tarjeta 2</span>
+						<p>Descripcion 2</p>
+					</div>
+					<div class="card-action">
+						<a href="#">Favoritos</a>
+						<a href="#">Retweet</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
