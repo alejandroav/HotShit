58d5ab05a1ce65d3d624d5918691e42d33282098
themarioga <marioga2512@gmail.com>
2015-10-04 19:42:17 +0200

    Conversor de video "acabado"
    Ez

--- src/operaciones.php
+++ src/operaciones.php
@@ -4,21 +4,17 @@
 			case "uploadvideo":
 				require ("libs/ffmpeg.class.php");
 				$FFmpeg = new FFmpeg;
-				$ds = DIRECTORY_SEPARATOR;
 				$storeFolder = 'uploads';
 				if (!empty($_FILES)) {
 					$tempFile = $_FILES['file']['tmp_name'];
 					$extension = pathinfo($tempFile, PATHINFO_EXTENSION);
 					$name = pathinfo($_FILES['file']['name'], PATHINFO_FILENAME);
-					$targetPath = dirname(__FILE__).$ds.$storeFolder.$ds;
+					$targetPath = dirname(__FILE__)."/".$storeFolder."/";
 					$targetFile = $targetPath.$name.".mp4";
 					if (file_exists($tempFile)) {
-						$FFmpeg->input($tempFile);
-						$FFmpeg->output($targetFile);
-						if ($FFmpeg->ready()){
-							if (file_exists($targetFile)) die (json_encode(array("status" => "OK", "msg" => "Original: ".$tempFile." Nuevo: ".$targetFile)));
-							else die (json_encode(array("status" => "ERROR", "msg" => "Error al copiar el archivo ".$targetFile)));
-						} else die (json_encode(array("status" => "ERROR", "msg" => "Error al convertir al archivo ".$targetFile)));
+						exec("/bin/ffmpeg -i $tempFile -c:v libx264 -c:a aac -strict -2 $targetFile"); 
+						if (file_exists($targetFile) && filesize($targetFile) > 0) die (json_encode(array("status" => "OK", "msg" => "Original: ".$tempFile." Nuevo: ".$targetFile)));
+						else die (json_encode(array("status" => "ERROR", "msg" => "Error al copiar el archivo ".$targetFile)));
 					} else die (json_encode(array("status" => "ERROR", "msg" => "Error al subir el archivo ".$tempFile)));
 				} else die (json_encode(array("status" => "ERROR", "msg" => "Debe seleccionar algun archivo")));
 			break;
