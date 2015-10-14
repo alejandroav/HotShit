9ebbdc5f3b26219b457e1cd7d9111a0aa4b07b0f
angelnm <angel.nm@hotmail.es>
2015-10-14 20:04:47 +0200

    Animacion subir

--- src/timelines.php
+++ src/timelines.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+   $_SESSION['userid'] = 0;
+     $_SESSION['username'] = "caca";
+     $_SESSION['userimg'] = "";
 if (!isset($_SESSION['userid']))
 	header('Location: index.php');
 ?>
@@ -12,15 +15,16 @@ if (!isset($_SESSION['userid']))
 		<title>Wezee</title>
 		<script src="resources/js/jquery-2.1.4.min"></script>
 		<script src="resources/js/materialize.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<link rel="stylesheet" href="resources/css/materialize.min.css"/>
 		<link rel="stylesheet" href="resources/css/frontline.css"/>
 	</head>
 	<body style="background-color:#FBFBFB">
-		<a name="top"><?php include("pages/header.php"); ?>
+		<a><?php include("pages/header.php"); ?>
 		<div style="padding:10px">
 			<?php include("pages/uploader.php"); ?>
-		</div>
-
+		</div>		
+		
 		<div class="row">
 			<div class="col s3">
 				<div class="align-center" style="padding:20px;">
@@ -31,8 +35,8 @@ if (!isset($_SESSION['userid']))
 				<?php include("pages/timelines.php"); ?>
 			</div>
 		</div>
-		<div class="fixed-action-btn" style="bottom:45px; right:24px;">
-			<a href="#top">
+		<div id="bt_top" class="fixed-action-btn" style="bottom:45px; right:24px;">
+			<a href="#">
 			 <i class="waves-effect waves-light btn boton_subir">Subir</i>
 			</a>
 		</div>
@@ -45,7 +49,7 @@ if (!isset($_SESSION['userid']))
 				var circle = '<div id="loadmore" class="preloader-wrapper active" style="margin-left: '+($("#timelines").width()/2)+'px;"><div class="spinner-layer spinner-green-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>';
 				$("#timelines").append(circle);
 				$.ajax({
-					url: 'pages/timelines.php',
+					url: 'pages/timelines.php', 
 					success: function(res) {
 						$("#timelines").append(res);
 						$("#loadmore").remove();
@@ -57,6 +61,7 @@ if (!isset($_SESSION['userid']))
 			];
 			Materialize.scrollFire(options);
 		</script>
+		<script src="resources/js/scroll.js"></script>
 		<script src="resources/js/uploader.js"></script>
 	</body>
 </html>
