4118cb590e2d2f8d8a8358b5024001b6585b984b
themarioga <marioga2512@gmail.com>
2015-10-15 18:52:55 +0200

    hola holita vecinitos de header

--- src/pages/header.php
+++ src/pages/header.php
@@ -1,13 +1,12 @@
 <header>
 	<nav>
-		<div class="nav-wrapper" style="background-color:#FFF700">
+		<div class="nav-wrapper" style="background-color:rgb(26,25,18)">
 			<a href="index.php" class="brand-logo" style="color:black;margin-left:15px">
-				<div style="color:yellow;padding:10px">
+				<div style="padding:10px">
 					<img src="resources/images/zlogo2_1.png" width="50px">
-					<img src="resources/images/zlogo.png" width="130px">
 				</div>
 			</a>
-			<ul id="nav-mobile" class="right hide-on-med-and-down">
+			<ul class="right hide-on-med-and-down">
 				<li>
 					<a href="#">
 					<div class="chip">
