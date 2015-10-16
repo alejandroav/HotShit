7fbf43d7b506062feb69756632e688bc05bf8dc9
Alexei-117 <alexei-117@hotmail.com>
2015-10-16 15:54:49 +0200

    puto alejandro muy confiado dice

--- src/pages/register.php
+++ src/pages/register.php
@@ -2,7 +2,7 @@
 	<input type="text" name="user" placeholder="Username" required="required"/><!-- Usuario -->
 	<input type="email" name="email" placeholder="E-mail" required="required"/> <!-- email -->
 	<input type="password" name="password" placeholder="Password" required="required"/><!-- contrasenya -->
-	<button class="btn waves-effect waves-light" type="submit" name="registrar" style="margin-top:-6px">
+	<button class="btn waves-effect waves-light" type="submit" name="registrar" style="margin-top:-8px">
 		Sign up <i class="material-icons right"></i>
 	</button>
 </form>
