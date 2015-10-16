59210fe039791da1fe38911f42bcfb4afe5b7d0d
Alejandro Alarcón <alejandroalarcon93@gmail.com>
2015-10-16 03:10:09 +0200

    a

--- src/pages/header.php
+++ src/pages/header.php
@@ -7,7 +7,7 @@
 			</ul>
 			<a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="mdi-navigation-menu"></i></a>
 			<script> $(".button-collapse").sideNav();</script>
-			<a href="index.php" class="brand-logo" style="color:black;margin-left:15px">
+			<a href="#" class="brand-logo" style="color:black;margin-left:15px">
 				<div style="margin-top:-6px">
 					<div class="chip chip_yellow">
 						<img src="uploads/userimg/<?php echo $_SESSION['userimg'];?>" alt="#"><?php echo $_SESSION['username']; ?>
@@ -15,10 +15,12 @@
 			</div>
 			</a>
 			<div class="brand-logo center">
+			<a href="index.php">
 				<div class="isotipo">
 					<img src="resources/images/zlogo2_3.png" width="120px" style="margin-top:-7px;">
 					<!--	<img src="resources/images/zlogo.png" width="130px"> -->
 				</div>
+			</a>
 			</div>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
 				<li>
