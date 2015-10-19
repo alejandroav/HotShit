dcba640a636b79c7d0c84355cbe5d96d3271974f
Alexei-117 <alexei-117@hotmail.com>
2015-10-19 11:32:51 +0200

    un sec

--- src/pages/recover.php
+++ src/pages/recover.php
@@ -1,3 +1,4 @@
+
 <form action="../operaciones.php?op=recover" method="POST" >
   <input type="email" name="email" required="required" placeholder="Email" style="border-radius:10px;top:50%;left:50%;"/>
   <button class="btn waves-effect waves-light" type="submit" name="ForgotPassword" style="margin-top:30px;border-radius:10px;top:50%;left:50%;background-color:rgb(255,236,50);font-family:'Impact',cursive;">Send recovery email</button>
