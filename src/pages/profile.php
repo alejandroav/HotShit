228b870289da82819761bdf31fe5ebe0336c86ac
Alexei-117 <alexei-117@hotmail.com>
2015-10-19 14:25:01 +0200

    wtffff

---
+++ src/pages/profile.php
@@ -0,0 +1,17 @@
+<form action="../operaciones.php method="POST" >
+	<div style="text-align:center">
+		<div>
+			<span style="font-family:verdana;font-size:120%;">Cambia tu foto: </span><a><button class="btn waves-effect waves-light" type="submit" name="subirFoto">Examinar</button></a>
+		</div>
+		<div style="font-family:verdana;font-size:120%;text-align:center;">
+			Borra uno de tus videos:
+		</div>
+		<div style="text-align:center">
+			<ul>
+				<li>
+					Video pepe <a><button class="btn waves-effect waves-light yellow" type="submit" name="borrarVideo">Examinar</button></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</form>
